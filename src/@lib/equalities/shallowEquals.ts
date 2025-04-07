23cd8a7725f5bbf7cd3a8ef863314bc0b665ca62
Suin Kim <contact@suinkim.me>
2025-04-07 23:23:47 +0900

    feat: 배열 얕게 비교

--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,3 +1,7 @@
 export function shallowEquals<T>(objA: T, objB: T): boolean {
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    return objA.every((value, index) => value === objB[index]);
+  }
+
   return objA === objB;
 }
