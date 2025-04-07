c4895c3f311e64e74a4c965cd9bfe82101f9b3d3
Suin Kim <contact@suinkim.me>
2025-04-07 23:36:31 +0900

    feat: 객체 얕은 비교

--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -3,5 +3,20 @@ export function shallowEquals<T>(objA: T, objB: T): boolean {
     return objA.every((value, index) => value === objB[index]);
   }
 
+  /**
+   * 코치님 발제 다시 보면서 타입스크립트에서 왜 이렇게 회피해야하는지 다시 확인하기
+   */
+
+  if (
+    typeof objA === "object" &&
+    typeof objB === "object" &&
+    objA !== null &&
+    objB !== null
+  ) {
+    const a = objA as { [key: string]: unknown };
+    const b = objB as { [key: string]: unknown };
+    return Object.keys(a).every((key) => a[key] === b[key]);
+  }
+
   return objA === objB;
 }
