de23f700abc4f012a46ad691e98f16126b28e4dd
Suin Kim <contact@suinkim.me>
2025-04-08 14:38:44 +0900

    feat: 깊은 비교

--- src/@lib/equalities/deepEquals.ts
+++ src/@lib/equalities/deepEquals.ts
@@ -1,3 +1,29 @@
+const isObject = (obj: unknown): obj is Record<string, unknown> => {
+  return obj !== null && typeof obj === "object";
+};
+
 export function deepEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  if (objA === objB) {
+    return true;
+  }
+
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    if (objA.length !== objB.length) {
+      return false;
+    }
+
+    return objA.every((value, key) => deepEquals(value, objB[key]));
+  }
+
+  if (isObject(objA) && isObject(objB)) {
+    const entriesA = Object.entries(objA);
+    const entriesB = Object.entries(objB);
+
+    return (
+      entriesA.length === entriesB.length &&
+      entriesA.every(([key, value]) => deepEquals(value, objB[key]))
+    );
+  }
+
+  return false;
 }
