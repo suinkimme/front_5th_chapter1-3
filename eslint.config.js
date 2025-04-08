4772d30ee7527352bcbbee85b07330bc0dbcf8b2
Suin Kim <contact@suinkim.me>
2025-04-09 02:15:52 +0900

    feat: useCallback 작성

--- eslint.config.js
+++ eslint.config.js
@@ -28,5 +28,5 @@ export default tseslint.config(
     },
   },
   eslintPluginPrettier,
-  eslintConfigPrettier,
+  eslintConfigPrettier
 );
--- src/@lib/hooks/useMemo.ts
+++ src/@lib/hooks/useMemo.ts
@@ -5,7 +5,7 @@ import { useRef } from "./useRef";
 export function useMemo<T>(
   factory: () => T,
   _deps: DependencyList,
-  _equals = shallowEquals
+  _equals = shallowEquals,
 ): T {
   const memoized = useRef<{ value: T; deps: DependencyList } | null>(null);
 
