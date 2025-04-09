70e5cb73a47b016a748b44887c385cfc65249d9c
Suin Kim <contact@suinkim.me>
2025-04-09 14:10:02 +0900

    chore: 배포 설정

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from "@vitejs/plugin-react-swc";
 
 export default mergeConfig(
   defineConfig({
+    base:
+      process.env.NODE_ENV === "production" ? "/front_5th_chapter1-3/" : "/",
     plugins: [react()],
   }),
   defineTestConfig({
@@ -16,5 +18,5 @@ export default mergeConfig(
         reporter: ["lcov", "json", "json-summary"],
       },
     },
-  }),
+  })
 );
